81c3b0883e4735a3a5b6289c66d66fc7406755b7
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-11-27 22:10:15 -0600

    feat: add MovementController

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}
