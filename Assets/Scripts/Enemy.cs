b77191a01817f995e21888c9e4d3a457e351459a
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-11-27 23:28:57 -0600

    feat: add enemies, die and shoot weapon behavior

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 2f;
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void DamageReceived(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TriggerWeapon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TriggerWeapon : MonoBehaviour
+{
+    public float damageAmount = 1f;
+
+    public float range = 150f;
+
+    public Camera PlayerCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(PlayerCam.transform.position, PlayerCam.transform.forward, out hit, range))
+        {
+            UnityEngine.Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                enemy.DamageReceived(damageAmount);
+            }
+        }
+    }
+}
